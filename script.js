04e501b12b1709108141e5aa15e693ba6c75c38c
dustinmclean <148822306+dustinmclean@users.noreply.github.com>
2025-03-28 09:39:06 -0700

    Create files, add navigation section, not including button styling

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("eat shit you ball huffer");
+
+function myButt(n) {
+  if (n < 5) {
+    console.log("I crapped me jeans");
+  } else {
+    console.log("I tooted");
+  }
+}
+
+console.log(myButt(4));
